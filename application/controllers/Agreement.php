568882daecfb180f71fadd6a3b11b060cceeccc4
MaulanaRizal <maulanarizal197@gmail.com>
2021-03-16 09:17:12 +0700

    update

--- application/controllers/Agreement.php
+++ application/controllers/Agreement.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Agreement extends CI_Controller{
 	function __construct(){
 		parent::__construct();
-		$this->load->model('m_agreement', 'agreement');	
+		$this->load->model('m_agreement', 'model');	
 	}
 	public function index()
 	{
@@ -17,5 +17,37 @@ class Agreement extends CI_Controller{
 	public function updateAgreement(){
 		$this->load->view('page/agreemnet/update');
 	}
+	public function insert()
+	{
+		// echo "----------> array POST : <br>";
+		// var_dump($_POST);
+		$data = array (
+			'NO_AGREEMENT' 		=> '',
+			'TANGGAL_AGREEMENT' => $_POST['agr_date'],
+			'TANGGAL_MULAI' 	=> $_POST['agr_mulai'],
+			'TANGGAL_BERAKHIR'	=> $_POST['agr_selesai'],
+			'IS_RENEWAL' 		=> $_POST['isRenewal'],
+			'JENIS_TAGIHAN' 	=> $_POST['agr_bill'],
+			'TANGGAL_POTONG' 	=> $_POST['agr_cut'],
+			'TIPE_PERIODE' 		=> $_POST['agr_period'],
+			'PERIODE' 			=> $_POST['agr-period-jml'],
+			'PER_PERIODE' 		=> $_POST['agr_period'],
+			'JANGKA_WAKTU' 		=> $_POST['agr_waktu'],
+			'AGREEMENT_TEKS' 	=> $_POST['agr_isi'],
+			'HUKUMAN' 			=> $_POST['agr_hukuman'],
+			'AKUN_BANK' 		=> '',
+			'REKENING' 			=> '',
+			'CRM_STATUS' 		=> $_POST['crm_status'],
+			'SBU_OWNER'			=> $_POST['crm_sbu'],
+			'ID_USER '			=> $_POST['crm_owner'],
+			'DESKRIPSI'			=> $_POST['crm_deskrip'],
+		);
+		// echo "----------> array data : <br>";
+		var_dump($data);
+		$this->model->insert('agreements',$data);
+		$this->session->set_flashdata('message',"<div class='alert alert-success'><strong>Berhasil!</strong>Ageement berhasil ditambahkan</div>");                        
+
+	}
+	
 }
 ?>
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -64,7 +64,7 @@
                             </div>
                         </div>
                         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <button class="btn btn-light me-md-5" name="login" type="submit">Button</button>
+                            <button class="btn btn-light me-md-5" name="login" type="submit">Login</button>
                             <!--<button class="btn btn-primary" type="button">Button</button>-->
                         </div>
             </form>
